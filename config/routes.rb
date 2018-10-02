729ef3e9a2f95d31453f1e1cf5c96fcfa16f74b5
Colin Armstrong <colinwarmstrong@gmail.com>
2018-10-01 20:53:35 -0600

    Add happy path for GET /api/v1/meals/:meal_id/foods which returns information about a specific meal and the foods associated with it.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :foods, only: [:index, :show, :create, :update, :destroy]
-      resources :meals, only: [:index]
+      resources :meals, only: [:index] do
+        get '/foods', to: 'meals#show'
+      end
     end
   end
 end
